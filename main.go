87d0bf3d030bfb92b4da2903953fd5835f6b3e7f
Daso <daso.timileyin@gmail.com>
2022-01-12 16:44:59 +0100

    database

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fiber-mongo-api/configs"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	App := fiber.New()
 
-	App.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
-	})
+	// App.Get("/", func(c *fiber.Ctx) error {
+	// 	return c.JSON(&fiber.Map{"data": "Hello from Fiber & mongoDB"})
+	// })
+
+	//run database
+	configs.ConnectDB()
+
 	App.Listen(":6000")
 }
