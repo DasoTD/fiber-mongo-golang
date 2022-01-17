a9fe564e7cd28f4ab1e30d1aa5c25febbf8d7f93
Daso <daso.timileyin@gmail.com>
2022-01-17 08:27:01 +0100

    post controller

---
+++ postController/post_controller.go
@@ -0,0 +1,118 @@
+package postController
+
+import (
+	"context"
+	"fiber-mongo-api/configs"
+	"fiber-mongo-api/models"
+	"fiber-mongo-api/responses"
+	"net/http"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var postCollection *mongo.Collection = configs.GetCollection(configs.DB, "posts")
+var validate = validator.New()
+
+func createPost(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var post models.Post
+	defer cancel()
+
+	//validate the request body
+	if err := c.BodyParser(&post); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.PostResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	newPost := models.Post{
+		ID:     primitive.NewObjectID(),
+		Title:  post.Title,
+		Body:   post.Body,
+		Author: post.Author,
+	}
+	result, err := postCollection.InsertOne(ctx, newPost)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.PostResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	return c.Status(http.StatusCreated).JSON(responses.PostResponse{Status: http.StatusCreated, Message: "success", Data: &fiber.Map{"data": result}})
+}
+
+func GetPost(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	postID := c.Params("postID")
+	var post models.Post
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(postID)
+
+	err := postCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&post)
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.PostResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	return c.Status(http.StatusOK).JSON(responses.PostResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": post}})
+}
+
+func EditPost(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	postID := c.Params("postID")
+	var post models.Post
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(postID)
+
+	//validate the request body
+	if err := c.BodyParser(&post); err != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.PostResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+
+	//use the validator library to validate required fields
+	if validationErr := validate.Struct(&post); validationErr != nil {
+		return c.Status(http.StatusBadRequest).JSON(responses.PostResponse{Status: http.StatusBadRequest, Message: "error", Data: &fiber.Map{"data": validationErr.Error()}})
+	}
+
+	update := bson.M{"title": post.Title, "body": post.Body, "author": post.Author}
+
+	result, err := postCollection.UpdateOne(ctx, bson.M{"id": objId}, bson.M{"$set": update})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.PostResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	//get updated post details
+	var updatedPost models.Post
+	if result.MatchedCount == 1 {
+		err := postCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&updatedPost)
+
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(responses.PostResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+		}
+	}
+
+	return c.Status(http.StatusOK).JSON(responses.PostResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": updatedPost}})
+
+}
+
+func DeletePost(c *fiber.Ctx) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	postID := c.Params("postID")
+	defer cancel()
+
+	objId, _ := primitive.ObjectIDFromHex(postID)
+	result, err := postCollection.DeleteOne(ctx, bson.M{"id": objId})
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(responses.PostResponse{Status: http.StatusInternalServerError, Message: "error", Data: &fiber.Map{"data": err.Error()}})
+	}
+	if result.DeletedCount < 1 {
+		return c.Status(http.StatusNotFound).JSON(
+			responses.PostResponse{Status: http.StatusNotFound, Message: "error", Data: &fiber.Map{"data": "User with specified ID not found!"}},
+		)
+	}
+
+	return c.Status(http.StatusOK).JSON(
+		responses.PostResponse{Status: http.StatusOK, Message: "success", Data: &fiber.Map{"data": "User successfully deleted!"}},
+	)
+
+}
